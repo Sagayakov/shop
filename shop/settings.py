ad11af32fd56c4d7c0ab723ba61cea29be78ce7d
Sagayakov <sadayfrom@gmail.com>
2023-03-23 15:59:09 +0500

    Добавил The Django Debug Toolbar

--- shop/settings.py
+++ shop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
     'shop_app.apps.ShopAppConfig',
     'feedback.apps.FeedbackConfig',
     'about_us.apps.AboutUsConfig',
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'shop.urls'
@@ -131,3 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
