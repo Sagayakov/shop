3e60b1691cd5651c469e11f3f93e67a78620e8fc
Sagayakov <sadayfrom@gmail.com>
2023-03-28 22:36:36 +0500

    Добавил тесты

--- feedback/tests.py
+++ feedback/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import FeedbackModel
+from .forms import FeedbackForm
+
+
+class FeedbackViewTestCase(TestCase):
+    def test_feedback_view_form_submission(self):
+        # Подготавливаем данные формы
+        form_data = {
+            'name': 'Test User',
+            'surname': 'Test',
+            'rating': '7',
+            'feedback': 'Test message'
+        }
+
+        # Отправляем POST-запрос с данными формы на обработку
+        response = self.client.post(reverse('feedback'), form_data)
+
+        # Проверяем, что форма была успешно отправлена и произошел редирект на страницу "done"
+        self.assertEqual(response.status_code, 302)
+        # self.assertRedirects(response, reverse('done'))
+
+        # Проверяем, что объект модели был создан в базе данных с указанными данными
+        feedback_test = FeedbackModel.objects.get(surname='Test')
+        self.assertEqual(feedback_test.name, 'Test User')
+        self.assertEqual(feedback_test.feedback, 'Test message')
+        self.assertEqual(feedback_test.rating, 7)
+
+    def test_feedback_view_form_display(self):
+        # Отправляем GET-запрос на страницу с формой обратной связи
+        response = self.client.get(reverse('feedback'))
+
+        # Проверяем, что страница содержит форму обратной связи
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<form')
+        self.assertIsInstance(response.context['form'], FeedbackForm)
+
+
+class DoneFeedbackViewTestCase(TestCase):
+    def test_done_feedback_view_display(self):
+        # Отправляем GET-запрос на страницу с сообщением об успешной отправке формы обратной связи
+        response = self.client.get(reverse('done'))
+
+        # Проверяем, что страница отображается корректно
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Отзыв успешно принят')
--- shop_app/tests.py
+++ shop_app/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from .models import MarkModel, PhoneModel
+
+
+class TestIndexView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('home')
+
+        # Создаем две марки телефонов и несколько моделей каждой марки
+        self.mark1 = MarkModel.objects.create(mark='Samsung', country='Корея', slug='samsung')
+        self.mark2 = MarkModel.objects.create(mark='Apple', country='США', slug='apple')
+        self.phone1 = PhoneModel.objects.create(mark_p=self.mark1, model_p='Galaxy S21', price_p=1000, slug='s21',
+                                                is_public=True)
+        self.phone2 = PhoneModel.objects.create(mark_p=self.mark1, model_p='Galaxy S20', price_p=800, slug='s20',
+                                                is_public=True)
+        self.phone3 = PhoneModel.objects.create(mark_p=self.mark2, model_p='iPhone 12', price_p=1200, slug='iphone-12',
+                                                is_public=True)
+
+    def test_index_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_pagination(self):
+        response = self.client.get(self.url + '?page=1')
+
+        # Проверяем, что пагинация работает корректно
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['phone']), 3)
+        self.assertTemplateUsed(response, 'shop_app/index.html')
+        self.assertEqual(response.context['page_obj'].number, 1)
+        self.assertEqual(len(response.context['page_obj'].object_list), 3)
+
+    def test_index_filter(self):
+        # Создаем несколько моделей телефонов, которые не публикуются
+        self.phone4 = PhoneModel.objects.create(mark_p=self.mark1, model_p='Galaxy S10', price_p=500, slug='s10',
+                                                is_public=False)
+        self.phone5 = PhoneModel.objects.create(mark_p=self.mark2, model_p='iPhone 11', price_p=1000, slug='iphone-11',
+                                                is_public=False)
+
+        response = self.client.get(self.url)
+
+        # Проверяем, что модели телефонов отображаются только если они публикуются
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['phone']), 3)
+        self.assertTemplateUsed(response, 'shop_app/index.html')
+        self.assertNotIn(self.phone4, response.context['phone'])
+        self.assertNotIn(self.phone5, response.context['phone'])
+
+
